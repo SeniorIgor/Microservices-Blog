e0f8c1c99e36bc3e08d1e02dc66619968b641011
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-11-24 19:54:05 +0600

    feat: created posts service

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,12 +1,25 @@
 import express, { Request, Response } from "express";
+import { randomBytes } from "crypto";
+
+import { Post } from "./types";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.POSTS_PORT || 4000;
+const posts: Record<string, Post> = {};
 
 app.get("/posts", (_req: Request, res: Response) => {
-  res.json([{ id: 1, title: "First post" }]);
+  res.send(posts);
+});
+
+app.post("/posts", (req: Request, res: Response) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = { id, title };
+
+  res.status(201).send(posts[id]);
 });
 
 app.listen(PORT, () => {
